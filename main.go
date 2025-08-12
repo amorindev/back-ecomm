3850f06dea4f60693cbe9758125b44afd0891607
Fernando Amorin <fernandoamorindev@gmail.com>
2025-08-12 11:26:10 -0500

    Add .env file and integrate godotenv for environment variable loading

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Printf("Hello world\n")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
 }
